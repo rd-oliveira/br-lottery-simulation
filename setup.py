a83a89d324dc4cc8664425165348fef2e2789cd7
rd-oliveira <doomsdayst9@gmail.com>
2024-09-08 15:44:14 -0300

    update requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="br-lottery-simulation",
-    version="0.0.6",
+    version="0.0.7",
     author="richard_oliveira",
     author_email="doomsdayst9@gmail.com",
     description="brazilian lottery game simulation",
